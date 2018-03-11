337d170ea92b9e6503054b0694b3347c8245fdd3
lucaszeng <lucaszeng99@gmail.com>
2018-03-10 20:29:48 -0700

    init

---
+++ src/components/Calculator.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="calculator">
+    <input type="text" v-model="calcInput">
+    <div class="calc-preview">
+      <p>{{ calcInput }}</p>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+      calcInput: '',
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Calculator from '@/components/Calculator'
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/calc',
+      name: 'Calculator',
+      component: Calculator
+    }
+  ]
+})
