325f5629ed1a617766b1d2d0c7fc2a124cbcc72f
lucaszeng <lucaszeng99@gmail.com>
2018-03-11 13:13:11 -0600

    finish multiple digits. todo: 小数点

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,43 +1,52 @@
 <template>
   <div class="calculator">
-    <input type="text" v-model="calcInput" @keyup.enter="calcAnswer">
+    <input type="text" v-model="input" @keyup.enter="calcAnswer">
     <div class="calc-preview">
-      <p>{{ calcInput }}</p>
       <p>{{ answer }}</p>
-      <p>{{ postFix }}</p>
+      <p>parsedInput: {{parsedInput}}</p>
     </div>
   </div>
 </template>
 
 
 <script>
-var parser = require('./lib/parser')
+var parser = require('./lib/parser').default
+
 export default {
   name: '',
   data () {
     return {
-      calcInput: '',
+      input: '',
       answer: null,
     }
   },
   methods: {
     calcAnswer: function () {
       if (this.postFix) {
-        this.answer = parser.default.reducePostfix(this.postFix)
+        this.answer = parser.reducePostfix(this.postFix)
       }
     }
   },
+  watch: {
+    input: function() {
+      this.answer = null
+    }
+  },
   computed: {
-    // answer: () => {
+    parsedInput: function () {
+      return parser.parseRawInput(this.input)
+    },
     postFix: function () {
-      return parser.default.toPostfix(this.calcInput)
+      this.answer = null
+      return parser.toPostfix(parser.parseRawInput(this.input))
       },
-    // }
   },
 
 }
 </script>
 
 <style scoped>
-
+input {
+  padding: 6px;
+}
 </style>
--- src/components/lib/parser.js
+++ src/components/lib/parser.js
@@ -1,9 +1,30 @@
+import { isNumber } from "util";
+
 // 不是topostfix 的问题，问题在于postfix不健全的时候reduce要崩溃。
 // 目标： 等待健全的postfix.
 var parser = {
-  test: function (x, y) {
-    console.log("x: " + x + " y: " + y)
+    /**
+   * parse all multiple digits.
+   * @param {string} rawInput
+   * @returns {string}
+   */
+  parseRawInput: (rawInput) => {
+    var i = 0
+    var output = []
+    while (i < rawInput.length) {
+      var j = 1
+      var num = [rawInput[i]]
+      // console.log(i + j < rawInput.length,!isNaN(parseInt(rawInput[i])), !isNaN(parseInt(rawInput[i+j])))
+      while (i + j < rawInput.length && !isNaN(parseInt(rawInput[i])) && !isNaN(parseInt(rawInput[i+j]))) {
+        num = num.concat(rawInput[i+j])
+        j++
+      }
+      output.push(num.join(''))
+      i = i + j 
+    } 
+    return output
   },
+
   toPostfix: (rawInput) => {
     var stack = []
     var prec = {
@@ -16,18 +37,17 @@ var parser = {
       ')': 1,
     }
     var output = []
+
     if (rawInput != null || rawInput != undefined) {
 
       for (let i in rawInput) {
-        // console.log(rawInput[i],Object.keys(prec))
+        console.log(rawInput[i])
         if (Number.isInteger(parseInt(rawInput[i]))) {
           output.push(rawInput[i])
         } 
-          
         else if (rawInput[i] == '(') {
           stack.push(rawInput[i])
         }
-
         else if (rawInput[i] == ')') {
           var topToken = stack.pop()
           for (let i = stack.length; i > 0; i--) {
@@ -42,7 +62,6 @@ var parser = {
           }
         }
         else {
-          // console.log(prec[stack[0]], prec[rawInput[i]], stack)
           while (stack && prec[stack[stack.length - 1]] >= prec[rawInput[i]]) {
             output.push(stack.pop())
           }
@@ -53,14 +72,14 @@ var parser = {
       while (stack && stack.length > 0) {
         output.push(stack.pop())
       }
-      console.log(output)
       return output
     }
     else {
       return null
     }
   },
-  reducePostfix: (postfix) => {
+  reducePostfix: (rawPostfix) => {
+    var postfix = rawPostfix
     var prec = {
       '*': 3,
       '/': 3,
@@ -71,8 +90,11 @@ var parser = {
       ')': 1,
     }
     for (let i in postfix) {
-      if (Number.isInteger(parseInt(postfix[i]))) {
+      if (!isNaN(parseInt(postfix[i]))) {
         postfix[i] = parseInt(postfix[i])
+      }
+      else if (!Object.keys(prec).includes(postfix[i])) {
+        return "your input might be wrong."
       }
     }
     while (postfix.length > 1) {
@@ -89,6 +111,11 @@ var parser = {
             postfix.splice(i-1, 2)
             break
           }
+          else if (postfix[i] == '^'){
+            postfix[i-2] = Math.pow(postfix[i-2], postfix[i-1])
+            postfix.splice(i-1, 2)
+            break
+          }
           else if (postfix[i] == '*'){
             postfix[i-2] *= postfix[i-1]
             postfix.splice(i-1, 2)
@@ -102,8 +129,13 @@ var parser = {
         }
       }
     }
+    if (!(postfix.length == 1) || !isNumber(postfix[0])) {
+      return "your input might be wrong."
+    }
     return postfix[0]
-  }
+  },
+    
+
 }
 
 export default parser
