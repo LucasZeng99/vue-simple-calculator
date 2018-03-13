f755e9eddbf84da370e9626f0b8e12a9e726bccd
lucaszeng <lucaszeng99@gmail.com>
2018-03-12 22:35:47 -0600

    add blog and github button

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
     <router-view/>
+    <div class="footer">
+        <a href="https://lucaszeng99.github.io/blog/" target="_blank">blog</a>
+        <a href="https://github.com/LucasZeng99/vue-simple-calculator" target="_blank">Visit on Github</a>
+    </div>
   </div>
+
 </template>
 
 <script>
@@ -24,4 +29,54 @@ body {
   background-attachment: fixed;
   background-size: cover;
 }
+
+div.footer {
+  position: absolute;
+  bottom: 20px;
+  right: 40px;
+  margin-bottom: 2px;
+}
+
+a {
+  display: inline-block;
+  padding:10px;
+  margin: 4px;
+  text-decoration: none;
+  color: rgb(220, 45, 255);
+  font-size: 24px;
+  transition: 0.6s;
+  margin-bottom: 2px;
+}
+a:hover {
+  margin-bottom: 0;
+  border-bottom: 2px solid #ff7525; 
+  animation-duration: 0.5s;
+  animation-name: jump;
+}
+
+@keyframes jump {
+  0% {
+    position: relative;
+    bottom: 0px;
+  }
+  30% {
+    position: relative;
+    bottom: 12px;
+  }
+  60% {
+    position: relative;
+    bottom: 0px;
+  }
+  80% {
+    position: relative;
+    bottom: 3px;
+  }
+  100% {
+    position: relative;
+    bottom: 0px;
+  }
+}
+
+
+
 </style>
